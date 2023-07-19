a827b4785b86129539ff01e3956e73ced0be83bf
Mike Summers <msummers@newrelic.com>
2023-07-19 08:38:06 -0500

    v1.0.0

--- cmd/resource/errorHandler.go
+++ cmd/resource/errorHandler.go
@@ -1,9 +1,12 @@
 package resource
 
 import (
+   "fmt"
+   "github.com/newrelic/newrelic-cloudformation-resource-providers-common/cferror"
    "github.com/newrelic/newrelic-cloudformation-resource-providers-common/client/nerdgraph"
    "github.com/newrelic/newrelic-cloudformation-resource-providers-common/model"
    log "github.com/sirupsen/logrus"
+   "strings"
 )
 
 // ErrorHandler at a minimum provides access to the default error processing.
@@ -24,34 +27,31 @@ func NewErrorHandler(p *Payload) (h model.ErrorHandler) {
    return
 }
 
-// func (h *ErrorHandler) TypeSpecificError(data *[]byte, s string) (err error) {
-//    defer func() {
-//       log.Debugf("errorHandler.TypeSpecificError (shadowed): self: %p exit", h)
-//    }()
-//    log.Debugf("TypeSpecificError: %p enter", h)
-//    defer func() {
-//       log.Debugf("TypeSpecificError: %p returning %v", h, err)
-//    }()
-//    v, err := nerdgraph.FindKeyValue(*data, "error")
-//    log.Debugf("TypeSpecificError: found: %v %T", v, v)
-//    if err != nil {
-//       return
-//    }
-//    if v == nil {
-//       return
-//    }
-//
-//    errorMap := make(map[string]interface{})
-//    h.GetErrorMap(v, errorMap)
-//
-//    if errorMap == nil {
-//       log.Warnf("Empty errors array: %v+ %T", h, h)
-//       return
-//    }
-//    _type := fmt.Sprintf("%v", errorMap[h.M.GetErrorKey()])
-//    if strings.Contains(strings.ToLower(_type), "not_found") || strings.Contains(strings.ToLower(_type), "invalid_parameter") {
-//       err = fmt.Errorf("%w Not found", &cferror.NotFound{})
-//       return
-//    }
-//    return
-// }
+func (e *ErrorHandler) TypeSpecificError(data *[]byte, s string) (err error) {
+   log.Debugf("TypeSpecificError: %p enter", e)
+   defer func() {
+      log.Debugf("TypeSpecificError: %p returning %v", e, err)
+   }()
+   v, err := nerdgraph.FindKeyValue(*data, "errors")
+   log.Debugf("TypeSpecificError: found: %v %T", v, v)
+   if err != nil {
+      return
+   }
+   if v == nil {
+      return
+   }
+
+   errorMap := make(map[string]interface{})
+   e.GetErrorMap(v, errorMap)
+
+   if errorMap == nil {
+      log.Warnf("Empty errors array: %v+ %T", e, e)
+      return
+   }
+   _type := fmt.Sprintf("%v", errorMap[e.M.GetErrorKey()])
+   if strings.Contains(strings.ToLower(_type), "not_found") || strings.Contains(strings.ToLower(_type), "not found") {
+      err = fmt.Errorf("%w Not found", &cferror.NotFound{})
+      return
+   }
+   return
+}
